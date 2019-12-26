2f8d03590d0ea0d21c339d49636710d3ea2beacf
Jharell Chambers <jharell.chambers@selu.edu>
2019-12-26 04:00:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,15 +11,15 @@ module FormsLab
     erb :'pirates/new' 
   end 
   
-  post '/pirates' do 
-    @pirate = Pirate.new(params[:pirate])
-    
-    params[:pirate][:ships].each do |details|
-      Ship.new(details)
-    end 
-    
-    @ships = Ship.all
-    erb :'pirates/show'
-  end 
+  post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+      @ships = Ship.all
+
+      erb :'pirates/show'
+    end
  end 
 end
